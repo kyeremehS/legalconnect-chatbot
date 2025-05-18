9136e35af7c8b40f6dafbae898b8f2653af15461
Sam <affum3331@gmail.com>
2025-05-18 20:44:47 +0000

    requirements updated and template added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="legalconect-bot",  
+    version="0.0.0",
+    author="Samuel Kyeremeh",
+    author_email="affum3331@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+from setuptools import find_packages, setup
+
